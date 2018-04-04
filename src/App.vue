478d1da746507068cf31b13c4561b779acbf894d
nulad <nuladwisnupambudi@gmail.com>
2018-04-05 00:13:25 +0700

    another initial commit

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app" class="container my-5">
+    <h1>Candidate Showcase</h1>
+
+    <div class="row">
+      <Item
+        v-for="item in candidates"
+        :key="item.candidateId"
+        :invId="item.candidateId"
+        :name="item.name"
+        :image="item.image"
+        :dapil="item.dapil"
+        :komisi="item.komisi"
+        :summary="item.summary" />
+    </div>
+  </div>
+</template>
+
+<script>
+require("bootstrap/dist/css/bootstrap.min.css");
+import Item from "./Item.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      candidates: [
+        {
+          candidateId: 1,
+          name: "Ahmad Sahroni",
+          image: "src/assets/ahmad-sahroni.png",
+          dapil: "DKI Jakarta III",
+          komisi: "III",
+          summary: "lorem ipsum dolor sit amit2"
+        },
+        {
+          invId: 2,
+          name: "Ahmad Zainuddin",
+          image: "src/assets/ahmad-zainuddin.jpg",
+          dapil: "DKI Jakarta I",
+          komisi: "IX",
+          summary: "lorem ipsum dolor sit amit2"
+        },
+        {
+          invId: 3,
+          name: "Lucky Hakim",
+          image: "src/assets/lucky-hakim.jpg",
+          dapil: "Jawa Barat VI",
+          komisi: "X",
+          summary: "lorem ipsum dolor sit amit2"
+        },
+        {
+          invId: 4,
+          name: "Okky Asokawati",
+          image: "src/assets/okky-asokawati.jpg",
+          dapil: "DKI Jakarta II",
+          komisi: "IX",
+          summary: "lorem ipsum dolor sit amit2"
+        }
+      ]
+    };
+  },
+  components: {
+    Item
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col-md-3">
+        <div class="card">
+          <img :src="image" :alt="name" class="card-img-top">
+          <div class="card-body">
+            <h4 class="card-title">{{ name }}</h4>
+            <div class="card-text">Dapil: {{ dapil }}</div>
+            <div class="card-text">Komisi: {{ komisi }}</div>
+            <div class="card-text">{{ summary }}</div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'item',
+  props: ['id', 'name', 'image', 'dapil', 'komisi', 'summary']
+};
+</script>
+

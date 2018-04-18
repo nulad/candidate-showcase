03434c544a56a8df1d64d855f83cea40aab78fee
nulad.pambudi <nulad.pambudi@erraipasifik.com>
2018-04-18 17:43:24 +0700

    moved the data source out of App.vue

---
+++ src/candidates.js
@@ -0,0 +1,36 @@
+var candidates = [
+    {
+        candidateId: 1,
+        name: "Ahmad Sahroni",
+        image: "src/assets/ahmad-sahroni.png",
+        dapil: "DKI Jakarta III",
+        komisi: "III",
+        summary: "lorem ipsum dolor sit amit2"
+    },
+    {
+        invId: 2,
+        name: "Ahmad Zainuddin",
+        image: "src/assets/ahmad-zainuddin.jpg",
+        dapil: "DKI Jakarta I",
+        komisi: "IX",
+        summary: "lorem ipsum dolor sit amit2"
+    },
+    {
+        invId: 3,
+        name: "Lucky Hakim",
+        image: "src/assets/lucky-hakim.jpg",
+        dapil: "Jawa Barat VI",
+        komisi: "X",
+        summary: "lorem ipsum dolor sit amit2"
+    },
+    {
+        invId: 4,
+        name: "Okky Asokawati",
+        image: "src/assets/okky-asokawati.jpg",
+        dapil: "DKI Jakarta II",
+        komisi: "IX",
+        summary: "lorem ipsum dolor sit amit2"
+    }
+];
+
+export default candidates;
